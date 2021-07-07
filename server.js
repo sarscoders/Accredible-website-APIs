398ce1981273fc9ec92c180b70c420c6c45f4675
sarscoders <52663999+sarscoders@users.noreply.github.com>
2021-07-07 22:20:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const app = require('./app');
 const port = process.env.PORT || process.argv[2] || 3200;
 const server = http.createServer(app);
 server.listen(port);
-console.log("server is running on 3200");
+console.log("server is running on "+port);
