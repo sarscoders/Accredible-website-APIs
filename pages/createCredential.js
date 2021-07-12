d4a425bfca0c61055959d4c133dcdb1841945097
sarscoders <52663999+sarscoders@users.noreply.github.com>
2021-07-12 14:03:05 +0530

    Add files via upload

---
+++ pages/createCredential.js
@@ -0,0 +1,30 @@
+var express = require('express');
+const router = express.Router();
+router.route('/').post((req, res, next) => {
+var request = require('request');	
+const config = require('./config.json');
+var name = "\""+ req.body.name+  "\""; 
+var email = "\""+ req.body.Email1+  "\""; 
+var coursename = "\""+ req.body.coursename+  "\""; 
+var groupname = "\""+ req.body.groupname+  "\""; 
+var groupid = req.body.groupid;
+var decs = "\""+ req.body.groupname+  "\"";
+request({
+  method: 'POST',
+  uri: `${config.url}/v1/credentials`,
+  headers: {
+	'Content-Type': 'application/json',
+    Authorization: `Token token=${config.key}`
+  },
+  body: "{  \"credential\": {    \"recipient\": { \"name\": "+name+",  \"email\": "+email+" },  \"name\": "+coursename+",    \"group_name\": "+groupname+",    \"group_id\": "+groupid+",	\"description\": "+decs+" }}"
+}, function (error, response, body) {
+  console.log('Status:', response.statusCode);
+  console.log('Headers:', JSON.stringify(response.headers));
+  console.log('Response:', body);
+	res.render("success" , {msg: 'Your credential has been created successfully'});
+});
+});
+router.route('/').get((req, res, next) => {
+	res.render("credentials");
+});
+module.exports = router;
---
+++ pages/createGroup.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var request = require('request');
+const router = express.Router();
+router.route('/').post((req, res, next) => {
+const config = require('./config.json');
+var name = "\""+ req.body.name+  "\""; 
+var courseName = "\""+ req.body.coursename+  "\""; 
+var decs = "\""+ req.body.coursedec+  "\""; 
+var courseLink = "\""+ req.body.course_link+  "\""; 
+var designId = req.body.design_id;
+request({
+  method: 'POST',
+  uri: `${config.url}/v1/issuer/groups`,
+  headers: {
+	'Content-Type': 'application/json',
+    Authorization: `Token token=${config.key}`
+  },
+  body: "{  \"group\": {    \"name\": "+name+",    \"course_name\": "+courseName+",    \"course_description\": "+decs+",    \"course_link\": "+courseLink+",    \"language\": \"en\",    \"attach_pdf\": true, \"blockchain\": false,    \"certificate_design_id\": null,    \"badge_design_id\": null,    \"design_id\": "+designId+",    \"learning_outcomes\": [      \"Reading Books\",      \"Writing Essays\"    ]  }}"
+}, function (error, response, body) {
+  console.log('Status:', response.statusCode);
+  console.log('Headers:', JSON.stringify(response.headers));
+  console.log('Response:', body);
+  res.render("success" , {msg: 'Your group has been created successfully'});
+});
+});
+router.route('/').get((req, res, next) => {
+	res.render("Group");
+});
+module.exports = router;
