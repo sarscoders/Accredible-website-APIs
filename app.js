51c9d565171a84272e34bd5908d3ba1f94a88de4
sarscoders <52663999+sarscoders@users.noreply.github.com>
2021-07-12 14:09:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const app = express();
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+const createCredential = require('./pages/createCredential');
+const createGroup = require('./pages/createGroup');
 const genpdf = require('./pages/genpdf');
 const Home = require('./pages/Home');
 const apicred = require('./pages/apicred');
+app.use('/createCredential', createCredential);
+app.use('/createGroup', createGroup);
 app.use('/genpdf', genpdf);
 app.use('/', Home);
 app.use('/edit', apicred);
